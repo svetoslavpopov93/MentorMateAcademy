21576d11e127d4ff8a35b53774fed8269418c284
Svetoslav Popov <svetoslavpopov93@gmail.com>
2015-04-20 20:47:07 +0300

    Project added to the git

---
+++ ClassAssignment8_MusicStars/ClassAssignment8_MusicStars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClassAssignment8_MusicStars
+//
+//  Created by Student17 on 4/20/15.
+//  Copyright (c) 2015 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClassAssignment8_MusicStars/ClassAssignment8_MusicStars/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClassAssignment8_MusicStars
+//
+//  Created by Student17 on 4/20/15.
+//  Copyright (c) 2015 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
