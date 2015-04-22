9463c2fa83c329214357c5dade5693d9f4d547ea
Svetoslav Popov <svetoslavpopov93@gmail.com>
2015-04-22 22:17:48 +0300

    ClassAssignment9_Face uploaded to the git

---
+++ AutoLayoutClassExample/AutoLayoutClassExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutClassExample
+//
+//  Created by Student17 on 4/22/15.
+//  Copyright (c) 2015 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ClassAssignment9_Face/ClassAssignment9_Face/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClassAssignment9_Face
+//
+//  Created by Student17 on 4/22/15.
+//  Copyright (c) 2015 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
