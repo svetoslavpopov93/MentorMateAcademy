48048f83d7fc0290045404b8af991eecee4fdfc6
Svetoslav Popov <svetoslavpopov93@gmail.com>
2015-04-27 21:02:12 +0300

    Uploading ClassAssignment10_UserInterfaceInspector to the git.

---
+++ ClassAssignment10_UserInterfaceInspector/ClassAssignment10_UserInterfaceInspector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClassAssignment10_UserInterfaceInspector
+//
+//  Created by Student17 on 4/27/15.
+//  Copyright (c) 2015 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
