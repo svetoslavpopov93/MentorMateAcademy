61a92742d35055ff482e27738e293446bd3fd9f0
Svetoslav Popov <svetoslavpopov93@gmail.com>
2015-05-18 22:04:51 +0300

    Class assignment 14 added to the git

---
+++ ClassAssignment14_Notifications/ClassAssignment14_Notifications/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClassAssignment14_Notifications
+//
+//  Created by Student17 on 5/18/15.
+//  Copyright (c) 2015 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+
+@end
+
